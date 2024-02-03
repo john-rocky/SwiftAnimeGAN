605e533b49825a2844e763f4d65601f297d33db1
john-rocky <rockyshikoku@gmail.com>
2024-02-04 07:12:35 +0900

    new

--- Package.swift
+++ Package.swift
@@ -1,23 +1,25 @@
+// swift-tools-version:5.3
+
 import PackageDescription
 
 let package = Package(
-    name: "SwiftAnimeGANv2",
+    name: "SwiftAnimeGAN",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SwiftAnimeGANv2",
-            targets: ["SwiftAnimeGANv2"]),
+            name: "SwiftAnimeGAN",
+            targets: ["SwiftAnimeGAN"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftAnimeGANv2",
+            name: "SwiftAnimeGAN",
             resources: [
             .process("animeganHayao.mlmodelc")]
     ),
         .testTarget(
-            name: "SwiftAnimeGANv2Tests",
-            dependencies: ["SwiftAnimeGANv2"]),
+            name: "SwiftAnimeGANTests",
+            dependencies: ["SwiftAnimeGAN"]),
     ]
 )
